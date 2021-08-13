5c809bfd02a0bed34050321785f838f6446398f3
Fabio Echegaray <Fabio.EchegarayIturra@ucsf.edu>
2021-08-13 12:51:10 -0700

    Modify original scrip to download GO information for all genes that are in the interactome table.

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
 import os
+import sys
+import time
 
 import pandas as pd
 import sqlite3
 import bioservices
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+log = logging.getLogger(__file__)
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_rows', 50)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_colwidth', 100)
 
 
-def import_mappings_to_sqlite():
+def import_mappings_to_sqlite(db_sqlite_file, huri_tsv_file, mapping_tsv_file):
     # create database with interactome and ID mappings in SQL
-    os.remove('go-interactome.db')
+    if os.path.exists(db_sqlite_file):
+        os.remove(db_sqlite_file)
 
-    tsv_map_f = 'data/idmapping_selected.tab.example'
-    tsv_interactome_f = 'data/HuRI.tsv'
-
-    newdb = sqlite3.connect('go-interactome.db')
+    newdb = sqlite3.connect(db_sqlite_file)
 
     # populate interactome database
-    for df in pd.read_csv(tsv_interactome_f, sep='\t', chunksize=20, iterator=True, encoding='utf-8',
-                          names=['From', 'To']):
+    for df in pd.read_csv(huri_tsv_file, sep='\t', encoding='utf-8',
+                          chunksize=1e6, iterator=True,
+                          names=['p1', 'p2']):
         df.to_sql('interactome', newdb, index=False, if_exists='append')
 
     # populate ID mapping database from file
-    hdr = ['UniProtKB_AC ', 'UniProtKB_ID ', 'GeneID (EntrezGene) ', 'RefSeq ', 'GI ', 'PDB ', 'GO ', 'UniRef100 ',
-           'UniRef90 ', 'UniRef50 ', 'UniParc ', 'PIR ', 'NCBI_taxon ', 'MIM ', 'UniGene ', 'PubMed ', 'EMBL ',
-           'EMBL_CDS ', 'Ensembl ', 'Ensembl_TRS ', 'Ensembl_PRO ', 'Additional PubMed']
-    for df in pd.read_csv(tsv_map_f, sep='\t', chunksize=20, iterator=True, encoding='utf-8', names=hdr):
+    hdr = ['UniProtKB_AC', 'UniProtKB_ID', 'GeneID (EntrezGene)', 'RefSeq', 'GI', 'PDB', 'GO', 'UniRef100',
+           'UniRef90', 'UniRef50', 'UniParc', 'PIR', 'NCBI_taxon', 'MIM', 'UniGene', 'PubMed', 'EMBL',
+           'EMBL_CDS', 'Ensembl', 'Ensembl_TRS', 'Ensembl_PRO', 'Additional PubMed']
+    for df in pd.read_csv(mapping_tsv_file, sep='\t', encoding='utf-8',
+                          chunksize=1e6, iterator=True,
+                          names=hdr, usecols=['UniProtKB_AC', 'Ensembl'],
+                          dtype={'UniProtKB_AC': 'str', 'Ensembl': 'str'}):
+        print(df.size)
         df = df.rename(columns={c: c.replace(' ', '') for c in df.columns})  # Remove spaces from columns
 
         df.to_sql('mapping', newdb, index=False, if_exists='append')
@@ -38,38 +44,40 @@ def import_mappings_to_sqlite():
 
 
 if __name__ == '__main__':
+    huri_file = '/media/lab/Data/Fabio/Dev/Python-InteractomeGO/data/HuRI.tsv'
+    map_file = '/media/lab/Data/Fabio/Dev/Python-InteractomeGO/data/idmapping_selected.tab.gz'
+    db_sqlite = '/media/lab/Data/Fabio/Dev/Python-InteractomeGO/data/go-interactome.db'
 
     # load interactions from HuRI database in Ensembl format
-    huri = pd.read_csv('data/huri.tsv', sep='\t', header=0)
-    genes = set(huri.values.ravel())
-
-    print(genes)
-    print(len(genes))
-
-    import_mappings_to_sqlite()
-    db = sqlite3.connect('file:go-interactome.db?mode=rw', uri=True)
+    import_mappings_to_sqlite(db_sqlite, huri_file, map_file)
+    db = sqlite3.connect(db_sqlite)
     cur = db.cursor()
 
-    ensembl_id = "ENSG00000274474"
-    cur.execute("SELECT UniProtKB_AC FROM mapping WHERE Ensembl LIKE :ensmbl_id;",
-                {'ensmbl_id': b"%" + ensembl_id.encode('utf-8') + b"%"})
-    rows = list(cur)
-    print(rows)
+    cur.execute("SELECT DISTINCT p1 as p FROM interactome UNION SELECT DISTINCT p2 as p FROM interactome;")
+    all_genes = list(cur)
+    for (ensembl_id,) in all_genes:
+        cur.execute("SELECT UniProtKB_AC FROM mapping WHERE Ensembl LIKE :ensmbl_id;",
+                    {'ensmbl_id': b"%" + ensembl_id.encode('utf-8') + b"%"})
+        rows = list(cur)
+        if not rows:
+            log.warning(f"No results found for Ensembl ID {ensembl_id}.")
+            continue
+        uniprotkb_id = rows[0][0]
+        log.info(f"Downloading info of gene with Ensembl ID={ensembl_id}, UniProtKB={uniprotkb_id}.")
 
-    s = bioservices.QuickGO()
-    uniprotkb_id = rows[0][0]
-    current_page = 1
-    total_pages = 1
-    while current_page <= total_pages:
-        annotations = s.Annotation(geneProductId=uniprotkb_id, page=current_page)
-        df = pd.DataFrame(annotations['results'])
-        for col in ['withFrom', 'extensions', 'targetSets']:
-            df.loc[:, col] = df[col].apply(lambda r: str(r) if r is not None else None)
-        df.loc[:, 'Ensembl'] = [ensembl_id] * len(df)
-        print(df)
-        df.to_sql('annotations', db, index=False, if_exists='append')
-        total_pages = annotations['pageInfo']['total']
-        print(f"Page {current_page}/{total_pages}")
-        current_page += 1
+        s = bioservices.QuickGO()
+        current_page = 1
+        total_pages = 1
+        while current_page <= total_pages:
+            annotations = s.Annotation(geneProductId=uniprotkb_id, page=current_page)
+            df = pd.DataFrame(annotations['results'])
+            for col in ['withFrom', 'extensions', 'targetSets']:
+                df.loc[:, col] = df[col].apply(lambda r: str(r) if r is not None else None)
+            df.loc[:, 'Ensembl'] = [ensembl_id] * len(df)
+            df.to_sql('annotations', db, index=False, if_exists='append')
+            total_pages = annotations['pageInfo']['total']
+            log.debug(f"Page {current_page}/{total_pages}")
+            current_page += 1
+            time.sleep(0.4)
 
     db.close()
