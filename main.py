c614904d4a53c0ed16da0f395d9a86c4ef11be05
Fabio Echegaray <Fabio.EchegarayIturra@ucsf.edu>
2021-08-15 12:16:02 -0700

    Add gene name to database; name is extracted from biggest mapping file (called raw in the script). Avoid calling QuickGO and inserting data if entry is already in DB.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import csv
+import gzip
 import time
 
 import pandas as pd
@@ -8,7 +10,7 @@ import bioservices
 import logging
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
-log = logging.getLogger(__file__)
+log = logging.getLogger('GO-db')
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_rows', 50)
 pd.set_option('display.max_columns', 500)
@@ -34,7 +36,7 @@ def import_mappings_to_sqlite(db_sqlite_file, huri_tsv_file, mapping_tsv_file):
            'EMBL_CDS', 'Ensembl', 'Ensembl_TRS', 'Ensembl_PRO', 'Additional PubMed']
     for df in pd.read_csv(mapping_tsv_file, sep='\t', encoding='utf-8',
                           chunksize=1e6, iterator=True,
-                          names=hdr, usecols=['UniProtKB_AC', 'Ensembl'],
+                          names=hdr, usecols=['UniProtKB_AC', 'Ensembl', ''],
                           dtype={'UniProtKB_AC': 'str', 'Ensembl': 'str'}):
         print(df.size)
         df = df.rename(columns={c: c.replace(' ', '') for c in df.columns})  # Remove spaces from columns
@@ -43,26 +45,54 @@ def import_mappings_to_sqlite(db_sqlite_file, huri_tsv_file, mapping_tsv_file):
     newdb.close()
 
 
+def import_raw_mappings_to_sqlite(db_sqlite_file, raw_mapping_tsv_file):
+    con = sqlite3.connect(db_sqlite_file)
+    cur = con.cursor()
+    cur.execute("DROP TABLE IF EXISTS rawmap;")
+    cur.execute('''CREATE TABLE rawmap (
+                    "UniProtKB_AC"	TEXT,
+                    "ID_type"	TEXT,
+                    "ID"	TEXT
+                );''')
+    a_file = gzip.open(raw_mapping_tsv_file, 'rt')
+    rows = csv.reader(a_file, delimiter='\t')
+    rows_to_insert = list()
+    for r in rows:
+        if 'Gene_Name' in r:
+            log.debug(f"Inserting {r} into DB.")
+            rows_to_insert.append(r)
+    cur.executemany("INSERT INTO rawmap VALUES (?, ?, ?)", rows_to_insert)
+
+    con.commit()
+    con.close()
+
+
 if __name__ == '__main__':
     huri_file = '/media/lab/Data/Fabio/Dev/Python-InteractomeGO/data/HuRI.tsv'
     map_file = '/media/lab/Data/Fabio/Dev/Python-InteractomeGO/data/idmapping_selected.tab.gz'
+    raw_map_file = '/media/lab/Data/Fabio/Dev/Python-InteractomeGO/data/idmapping.dat.gz'
     db_sqlite = '/media/lab/Data/Fabio/Dev/Python-InteractomeGO/data/go-interactome.db'
 
     # load interactions from HuRI database in Ensembl format
     import_mappings_to_sqlite(db_sqlite, huri_file, map_file)
+    import_raw_mappings_to_sqlite(db_sqlite, raw_map_file)
     db = sqlite3.connect(db_sqlite)
     cur = db.cursor()
 
     cur.execute("SELECT DISTINCT p1 as p FROM interactome UNION SELECT DISTINCT p2 as p FROM interactome;")
-    all_genes = list(cur)
-    for (ensembl_id,) in all_genes:
-        cur.execute("SELECT UniProtKB_AC FROM mapping WHERE Ensembl LIKE :ensmbl_id;",
-                    {'ensmbl_id': b"%" + ensembl_id.encode('utf-8') + b"%"})
-        rows = list(cur)
-        if not rows:
+    all_genes = [r[0] for r in cur]
+    cur.execute("SELECT DISTINCT Ensembl FROM annotations;")
+    already_annotated_genes = [r[0] for r in cur]
+    cur.execute("SELECT Ensembl, UniProtKB_AC FROM mapping WHERE Ensembl LIKE 'ENSG%';")
+    all_maps = {r[0]: r[1] for r in cur}
+    for ensembl_id in all_genes:
+        if not ensembl_id in all_maps.keys():
             log.warning(f"No results found for Ensembl ID {ensembl_id}.")
             continue
-        uniprotkb_id = rows[0][0]
+        if ensembl_id in already_annotated_genes:
+            log.warning(f"Skipping Ensembl ID {ensembl_id} because it's already annotated.")
+            continue
+        uniprotkb_id = all_maps[ensembl_id]
         log.info(f"Downloading info of gene with Ensembl ID={ensembl_id}, UniProtKB={uniprotkb_id}.")
 
         s = bioservices.QuickGO()
@@ -72,12 +102,13 @@ if __name__ == '__main__':
             annotations = s.Annotation(geneProductId=uniprotkb_id, page=current_page)
             df = pd.DataFrame(annotations['results'])
             for col in ['withFrom', 'extensions', 'targetSets']:
-                df.loc[:, col] = df[col].apply(lambda r: str(r) if r is not None else None)
+                if col in df.columns:
+                    df.loc[:, col] = df[col].apply(lambda r: str(r) if r is not None else None)
             df.loc[:, 'Ensembl'] = [ensembl_id] * len(df)
             df.to_sql('annotations', db, index=False, if_exists='append')
             total_pages = annotations['pageInfo']['total']
             log.debug(f"Page {current_page}/{total_pages}")
             current_page += 1
             time.sleep(0.4)
-
+    db.commit()
     db.close()
